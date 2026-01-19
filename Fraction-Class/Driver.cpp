939acf84c2484d1017b78d406d96f813f72726ec
novak727 <devinkennedy727@gmail.com>
2026-01-19 01:15:52 -0600

    Create Driver.cpp

---
+++ Fraction-Class/Driver.cpp
@@ -0,0 +1,20 @@
+//CS255 Class Tinkering 02/05/2025
+//Incomplete
+
+#include "Fraction.h"
+
+int main() {
+	Fraction f1(3,4);
+	Fraction f2(5,4);
+
+	cout << f1 << endl;
+	cout << ++f1 << endl;
+	cout << f1 << endl;
+
+	cout << f2 << endl;
+	cout << --f2 << endl;
+	cout << f2 << endl;
+	
+	return 0;
+}
+
